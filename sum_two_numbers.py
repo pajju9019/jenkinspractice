702a1eeefafcfc216a090b7013bee22dcbe7ab59
Ubuntu <ubuntu@ip-172-31-47-237.ap-south-1.compute.internal>
2026-09-04 04:34:36 +0000

    Add Docker Compose and Jenkins pipeline

--- sum_two_numbers.py
+++ sum_two_numbers.py
@@ -1,2 +1,20 @@
-print("prajwal git")
-print("dont add print change copilot")
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+
+a = 10
+b = 5
+
+print("Addition:", add(a, b))
+print("Subtraction:", subtract(a, b))
+print("Multiplication:", multiply(a, b))
+print("Division:", divide(a, b))
